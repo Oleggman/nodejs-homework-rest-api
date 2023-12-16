aa798c437cf06a0a76551f5b0e002d4eceabd09a
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-12-16 23:17:01 +0200

    create contact controller decorator

---
+++ controllers/contacts-controller.js
@@ -0,0 +1,40 @@
+import * as contactsService from '../models/contacts.js';
+import ctrlWrapper from '../decorators/ctrlWrapper.js';
+
+const getMovies = async (req, res, next) => {
+  const contacts = await contactsService.listContacts();
+  res.json(contacts)
+}
+
+const getMovieById = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await contactsService.getContactById(contactId);
+  res.json(contact)
+}
+
+const addMovie = async (req, res, next) => {
+  const { name, email, phone } = req.body;
+  const newContact = await contactsService.addContact(name, email, phone);
+  res.status(201).json(newContact);
+}
+
+const updateMovie = async (req, res, next) => {
+  const { contactId } = req.params;
+  const body = req.body;
+  const newContact = await contactsService.updateContact(contactId, body);
+  res.json(newContact);
+}
+
+const deleteMovie = async (req, res, next) => {
+  const { contactId } = req.params;
+  await contactsService.removeContact(contactId);
+  res.json({ "message": "contact deleted" });
+}
+
+export default {
+  getMovies: ctrlWrapper(getMovies),
+  getMovieById: ctrlWrapper(getMovieById),
+  addMovie: ctrlWrapper(addMovie),
+  updateMovie: ctrlWrapper(updateMovie),
+  deleteMovie: ctrlWrapper(deleteMovie),
+}
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,36 +1,16 @@
 import express from 'express';
-import * as contactsService from '../../models/contacts.js';
+import contactsController from '../../controllers/contacts-controller.js';
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  const contacts = await contactsService.listContacts();
-  res.json(contacts)
-})
+router.get('/', contactsController.getMovies);
 
-router.get('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await contactsService.getContactById(contactId);
-  res.json(contact)
-})
+router.get('/:contactId', contactsController.getMovieById);
 
-router.post('/', async (req, res, next) => {
-  const { name, email, phone } = req.body;
-  const newContact = await contactsService.addContact(name, email, phone);
-  res.status(201).json(newContact)
-})
+router.post('/', contactsController.addMovie);
 
-router.delete('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  await contactsService.removeContact(contactId);
-  res.json({"message": "contact deleted"})
-})
+router.delete('/:contactId', contactsController.deleteMovie);
 
-router.put('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  const body = req.body;
-  const newContact = await contactsService.updateContact(contactId, body);
-  res.json(newContact)
-})
+router.put('/:contactId', contactsController.updateMovie);
 
 export default router;
