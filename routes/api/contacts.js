6c06ab7f0253dd06461806b1a28b005086c1117a
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-12-17 00:12:01 +0200

    Add body validation with joi

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import contactsController from '../../controllers/contacts-controller.js';
 import isEmptyBody from '../../middlewares/isEmptyBody.js';
+import validateBody from '../../decorators/validateBody.js';
+import { contactAddSchema, contactUpdateSchema } from '../../schemas/contact-schemas.js';
 
 const router = express.Router();
 
@@ -8,9 +10,9 @@ router.get('/', contactsController.getMovies);
 
 router.get('/:contactId', contactsController.getMovieById);
 
-router.post('/', isEmptyBody, contactsController.addMovie);
+router.post('/', isEmptyBody, validateBody(contactAddSchema), contactsController.addMovie);
 
-router.put('/:contactId', isEmptyBody, contactsController.updateMovie);
+router.put('/:contactId', isEmptyBody, validateBody(contactUpdateSchema), contactsController.updateMovie);
 
 router.delete('/:contactId', contactsController.deleteMovie);
 
